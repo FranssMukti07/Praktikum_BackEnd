f016040c70077c3ed0598e2a238549671bb90025
FranssMukti07 <gigihzhafrans111@gmail.com>
2022-12-13 14:28:17 +0700

    initial commit

--- praktikum6/pertemuan10/download.js
+++ praktikum6/pertemuan10/download.js
@@ -1,16 +1,15 @@
 /**
  * Fungsi untuk menampilkan hasil download
  * @param {string} result - Nama file yang didownload
- */
 function showDownload(result) {
     console.log("Download selesai");
     console.log("Hasil Download: " + result);
 }
+*/
 
 /**
  * Fungsi untuk download file
  * @param {function} callback - Function callback show
- */
 function download(callShowDownload) {
     setTimeout(function () {
         const result = "windows-10.exe";
@@ -19,6 +18,7 @@ function download(callShowDownload) {
 }
 
 download(showDownload);
+*/
 
 /**
  * TODO:
@@ -26,3 +26,33 @@ download(showDownload);
  * - Refactor function ke ES6 Arrow Function
  * - Refactor string ke ES6 Template Literals
  */
+
+const download = () => {
+    return new Promise((resolve, reject) => {
+        const status = true;
+
+        setTimeout(() => {
+            if (status) {
+                resolve("Download sedang berlangsung...");
+            } else {
+                reject("Download Gagal");
+            }
+        }, 1000);
+    });
+};
+
+const showDownload = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const result = "Windows-10.iso";
+            resolve(`Download selesai\nHasil Download : ${result}`);
+        }, 5000);
+    });
+}
+
+async function main() {
+    console.log(await download());
+    console.log(await showDownload());
+}
+
+main();
